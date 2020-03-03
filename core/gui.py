8b4bb45e1897c750d7a9f97e764216583057231b
ricardo <rmedina0531@gmail.com>
2020-03-02 18:35:05 -0800

    Working with update gui file

--- core/gui.py
+++ core/gui.py
@@ -1,16 +1,21 @@
 
-# This file contains code that implements the GUI.
-
 import os
 
 import pygame as pg
-from pygame.time import Clock
+from pygame.color import Color
+from pygame.draw import line
+from pygame.font import SysFont
+from pygame.rect import Rect
+from pygame.sprite import Sprite
+from pygame.surface import Surface
 
 # By importing this file itself, can avoid the use of globals
 # noinspection PyUnresolvedReferences
 import core.gui as gui
 
 import PySimpleGUI as sg
+
+from typing import Optional, Tuple, Union
 
 import tkinter as tk
 
@@ -22,6 +27,12 @@ import tkinter as tk
 PATCH_SIZE = 11
 
 
+# BOARD_SHAPE is the shape of the board
+PATCH_ROWS = 51
+PATCH_COLS = 51
+
+
+
 def BLOCK_SPACING():
     return PATCH_SIZE + 1
 
@@ -30,12 +41,9 @@ def HALF_PATCH_SIZE():
     return PATCH_SIZE//2
 
 
-PATCH_ROWS = 51
-PATCH_COLS = 51
 
-
-def HOR_SEP(length=25):
-    return [sg.Text('_' * length, text_color='black')]
+def HOR_SEP(length=25, pad=((0, 0), (10, 10))):
+    return [sg.Text('_' * length, text_color='black', pad=pad)]
 
 
 
@@ -56,23 +64,45 @@ def SCREEN_PIXEL_HEIGHT():
 FPS_VALUES = values = [1, 3, 6, 10, 15, 25, 40, 60]
 
 
+def gui_set(key, **kwargs):
+    widget = WINDOW[key]
+    widget.update(**kwargs)
+
+
 def set_fps(val):
     # Select the value in FPS_VALUES closest to val.
-    new_val = min([(abs(v - val),  v) for v in FPS_VALUES])[1]
-    WINDOW['fps'].update(new_val)
+    new_val = min(FPS_VALUES, key=lambda v: abs(v - val))
+    # WINDOW['fps'].update(value=new_val)
+    gui_set('fps', value=new_val)
 
 
-# This variable will be available to refer to the SCREEN object from elsewhere in the code.
-# Note that it can't be imported directly because imports occur before the SCREEN is created.
-SCREEN = None
-WINDOW = None
+# The WINDOW variable will be available to refer to the WINDOW object from elsewhere in the code.
+# Neither the WINDOW nor the SCREEN can be imported directly because imports occur before they are created.
+WINDOW: sg.PySimpleGUI.Window
+
+
+# The following are from pygame.
+SCREEN: Surface
+FONT: SysFont
+
+
+# These pygame functions draw to the SCREEN, which is a pygame Surface.
+def blit(image: Surface, rect: Union[Rect, Tuple]):
+    gui.SCREEN.blit(image, rect)
+
+
+def draw_line(start_pixel, end_pixel, line_color: Color = Color('white'), width: int = 1):
+    line(gui.SCREEN, line_color, start_pixel, end_pixel, width)
 
 
 class SimpleGUI:
 
-    def __init__(self, model_gui_elements, caption="Basic Model", patch_size=15, bounce=None, fps=None):
+    def __init__(self, gui_left_upper, gui_right_upper=None, caption="Basic Model",
+                 patch_size=15, board_rows_cols=(51, 51), bounce=None, fps=None):
 
-        gui.PATCH_SIZE = patch_size
+        gui.PATCH_SIZE = patch_size if patch_size % 2 == 1 else patch_size + 1
+        gui.PATCH_ROWS = board_rows_cols[0] if board_rows_cols[0] % 2 == 1 else board_rows_cols[0] + 1
+        gui.PATCH_COLS = board_rows_cols[1] if board_rows_cols[1] % 2 == 1 else board_rows_cols[1] + 1
 
         self.EXIT = 'Exit'
         self.GO = 'go'
@@ -82,30 +112,31 @@ class SimpleGUI:
         self.SETUP = 'setup'
         self.STOP = 'Stop'
 
-        self.clock = Clock()
+        self.clock = pg.time.Clock()
         self.fps = 60
         self.idle_fps = 10
 
         self.screen_color = pg.Color(sg.RGB(50, 60, 60))
 
         self.caption = caption
-        self.model_gui_elements = model_gui_elements
 
-        screen_shape_width_height = (SCREEN_PIXEL_WIDTH(), SCREEN_PIXEL_HEIGHT())
+        self.screen_shape_width_height = (SCREEN_PIXEL_WIDTH(), SCREEN_PIXEL_HEIGHT())
 
         pg.init()
-        #pg.init causes program crash if called after the following line
-        gui.WINDOW = self.make_window(caption, model_gui_elements, screen_shape_width_height, bounce=bounce, fps=fps)
+
+        # All these gui.<variable> elements are globals in this file.
+        gui.WINDOW = self.make_window(caption, gui_left_upper, gui_right_upper=gui_right_upper, bounce=bounce, fps=fps)
 
         # pg.init()
+        gui.FONT = SysFont(None, int(1.5 * gui.BLOCK_SPACING()))
 
         # All graphics are drawn to gui.SCREEN, which is a global variable.
-        gui.SCREEN = pg.display.set_mode(screen_shape_width_height)
+        gui.SCREEN = pg.display.set_mode(self.screen_shape_width_height)
 
     def fill_screen(self):
-        SCREEN.fill(self.screen_color)
+        gui.SCREEN.fill(self.screen_color)
 
-    def make_window(self, caption, model_gui_elements, screen_shape_width_height, bounce=True, fps=None):
+    def make_window(self, caption, gui_left_upper, gui_right_upper=None, bounce=True, fps=None):
         """
         Create the window, including sg.Graph, the drawing surface.
         """
@@ -130,9 +161,10 @@ class SimpleGUI:
                       key=self.GOSTOP)   ]
 
 
-        exit_button_line = [sg.Exit(button_color=('white', 'firebrick4'), key=self.EXIT, pad=((70, 0), (10, 0)))]
+        exit_button_line = [sg.Exit(button_color=('white', 'firebrick4'), key=self.EXIT, pad=((0, 0), (10, 0))),
+                            sg.Checkbox('Grab anywhere', key='Grab', default=False, pad=((40, 0), (10, 0)))]
 
-        col1 = [ *model_gui_elements,
+        col1 = [ *gui_left_upper,
                  gui.HOR_SEP(),
                  setup_go_line,
                  bounce_checkbox_line,
@@ -141,25 +173,31 @@ class SimpleGUI:
                  exit_button_line
                  ]
 
-        lower_left_pixel_xy = (0, screen_shape_width_height[1]-1)
-        upper_right_pixel_xy = (screen_shape_width_height[0]-1, 0)
-        col2 = [[sg.Graph(screen_shape_width_height, lower_left_pixel_xy, upper_right_pixel_xy,
-                          background_color='black', key='-GRAPH-', enable_events=True)]]
+        lower_left_pixel_xy = (0, self.screen_shape_width_height[1]-1)
+        upper_right_pixel_xy = (self.screen_shape_width_height[0]-1, 0)
+
+        if gui_right_upper is None:
+            gui_right_upper = [[]]
+
+        # graph is a drawing area, a screen on which the model is portrayed, i.e., the patches and the agents.
+        # It consists mainly of a TKCanvas.
+        graph = sg.Graph(self.screen_shape_width_height, lower_left_pixel_xy, upper_right_pixel_xy,
+                         background_color='black', key='-GRAPH-', enable_events=True)
+        col2 = gui_right_upper + [[graph]]
 
         # layout is the actual layout of the window. The stuff above organizes it into component parts.
         # col1 is the control buttons, sliders, etc.
-        # col2 is the screen on which the model is portrayed, i.e., the patches and the agents.
+        # col2 is the graph plus whatever the user wants to put above it.
         # layout is a single "GUI line" with these two components in sequence.
         layout = [[sg.Column(col1), sg.Column(col2)]]
 
-        window: sg.PySimpleGUI.Window = sg.Window(caption, layout, margins=(5, 20),
-                                                  use_default_focus=False,
-                                                  return_keyboard_events=True, finalize=True)
-        graph: sg.PySimpleGUI.Graph = window['-GRAPH-']
+        # window is a window with that layout.
+        window = sg.Window(caption, layout, margins=(5, 20), use_default_focus=False, grab_anywhere=False,
+                           return_keyboard_events=True, finalize=True)
 
         # -------------- Magic code to integrate PyGame with tkinter -------
-        embed: tk.Canvas = graph.TKCanvas
-        os.environ['SDL_WINDOWID'] = str(embed.winfo_id( ))
+        w_id = graph.TKCanvas.winfo_id( )
+        os.environ['SDL_WINDOWID'] = str(w_id)
         os.environ['SDL_VIDEODRIVER'] = 'windib'  # change this to 'x11' to make it work on Linux
 
         return window
