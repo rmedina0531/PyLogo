ec7cc5ff7f7d8e069664614bd37608ecca09508c
Ricardo <rmedina0531@gmail.com>
2020-04-10 12:24:24 -0700

    Added Middle road and Checkbox for Middle road

--- Examples/braess_road.py
+++ Examples/braess_road.py
@@ -3,6 +3,7 @@ from core.world_patch_block import World, Patch
 from core.agent import Agent
 from pygame.color import Color
 from random import randint
+from core.sim_engine import SimEngine
 
 class Braess_Road_Patch(Patch):
     def __init__(self, *args, **kwargs):
@@ -46,7 +47,7 @@ class Braess_Road_World(World):
 
         # world class might have a ticks attribute
         self.ticks = None
-
+        self.middle_on = False
     def setup(self):
         # Clear everything
         self.reset_all()
@@ -117,12 +118,34 @@ class Braess_Road_World(World):
             self.patches_array[i, PATCH_ROWS - 3].set_color(Color('Yellow'))
             self.patches_array[i, PATCH_ROWS - 3].set_road_type(road_type)
 
-        if self.middle_on:
-            pass
+            if SimEngine.gui_get(MIDDLE_ON):
+                # self.patches_array[PATCH_ROWS - ,]
+                # [PATCH_ROWS - 3][2]
+                i = 0
+                for i in range(44):
+                    print(i)
+                    x = PATCH_ROWS - 4 - i
+                    y = 3 + i
+                    diagonal_patch = self.patches_array[x][y]
+                    top_patch = self.patches_array[x-1][y]
+                    left_patch = self.patches_array[x][y+1]
+                    diagonal_patch.set_color(Color('Orange'))
+                    top_patch.set_color(Color('Grey'))
+                    left_patch.set_color(Color('Grey'))
+                    if i == 43:
+                        self.patches_array[x-1][y+1].set_color(Color('Orange'))
+                #set the last patch
 
 # ############################################## Define GUI ############################################## #
 import PySimpleGUI as sg
-gui_left_upper = [ [sg.Text('nbr agents', pad=((0, 5), (20, 0))),
+MIDDLE_ON = 'middle_on'
+
+
+# switches = [sg.CB(n + '\n 1', key=n, pad=((30, 0), (0, 0)), enable_events=True)
+#                                              for n in reversed(CA_World.bin_0_to_7)]
+gui_left_upper = [[sg.Text('Middle On?', pad=((0,5), (20,0))), sg.CB('True', key=MIDDLE_ON, pad=((0,5), (20,0)))]]
+
+gui_left_upperw = [ [sg.Text('Middle on', pad=((0, 5), (20, 0))),
                     sg.Slider(key='nbr_agents', range=(1, 101), resolution=25, default_value=25,
                               orientation='horizontal', size=(10, 20))] ]
 
@@ -130,4 +153,4 @@ gui_left_upper = [ [sg.Text('nbr agents', pad=((0, 5), (20, 0))),
 if __name__ == "__main__":
     from core.agent import PyLogo
     # PyLogo(Braess_Road_World, 'Braess Road Paradox', gui_left_upper, bounce=True, patch_size=9, board_rows_cols=(71, 71))
-    PyLogo(world_class=Braess_Road_World, caption='Braess Road Paradox', agent_class=Commuter, patch_class=Braess_Road_Patch)
+    PyLogo(world_class=Braess_Road_World, caption='Braess Road Paradox', agent_class=Commuter, gui_left_upper=gui_left_upper, patch_class=Braess_Road_Patch)
